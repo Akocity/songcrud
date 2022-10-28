68c6f17ffabfaa8e9a60dc47cfb5fa5b58916f0a
Ugbo Joy <ak00c1ty@gmail.com>
2022-10-28 21:50:59 +0100

    updated models

--- musicapp/models.py
+++ musicapp/models.py
@@ -7,8 +7,8 @@ class Artiste(models.Model) :
     last_name = models.CharField(max_length=40)
     age = models.IntegerField()
 
-    # def __str__ (self)
-    # return f"{self.first_name} {self.last_name}"
+    def __str__ (self) :
+        return f"{self.first_name} {self.last_name}"
 
 class Song(models.Model) :
     title = models.CharField(max_length=50)
@@ -16,9 +16,12 @@ class Song(models.Model) :
     likes = models.IntegerField()
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
 
-    # def __str__ (self) :
-    #  return f"{self.title}"
+    def __str__ (self) :
+        return self.artiste_id.first_name
 
 class Lyrics(models.Model) :
     content = models.TextField(max_length=1000)
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__ (self) :
+        return self.song_id.title
